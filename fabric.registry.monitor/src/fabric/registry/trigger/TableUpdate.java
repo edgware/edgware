f845ac6215624fc8ef591f4719cde9e59c043518
Christopher Gibson <christopher.gibson@uk.ibm.com>
2014-09-25 16:25:28 +0100

    New function to include a systems services in its trigger message.

--- fabric.registry.monitor/src/fabric/registry/trigger/TableUpdate.java
+++ fabric.registry.monitor/src/fabric/registry/trigger/TableUpdate.java
@@ -9,6 +9,11 @@
 
 package fabric.registry.trigger;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.logging.Logger;
 
 /**
@@ -59,15 +64,32 @@ public class TableUpdate {
 				resourceManager = ResourceManager.getInstance();
 			}
 
+			StringBuilder notification = new StringBuilder('{');
+			StringBuilder notificationDetail = new StringBuilder();
+
 			if (resourceManager.isReportable(tableName)) {
 
-				/* Build the notification message (a JSON object) */
-				String notification = String.format(
-						"{\"table\":\"%s\",\"key\":\"%s\",\"id\":\"%s\",\"action\":\"%s\",\"timestamp\":%d}",
-						tableName, key, id, action, System.currentTimeMillis());
+				switch (tableName.toUpperCase()) {
+
+				case "SERVICES":
+
+					/* Lookup and record the feeds associated with this service */
+					String[] keys = key.split("/");
+					notificationDetail = query(keys[0], keys[1]);
+
+				default:
+
+					/* Build the notification message (a JSON object) */
+					notification.append(String.format(
+							"\"table\":\"%s\",\"key\":\"%s\",\"id\":\"%s\",\"action\":\"%s\",\"timestamp\":%d",
+							tableName, key, id, action, System.currentTimeMillis()));
+				}
+
+				notification.append(notificationDetail);
+				notification.append('}');
 
 				/* Publish to the Fabric */
-				resourceManager.sendRegistryUpdate(notification);
+				resourceManager.sendRegistryUpdate(notification.toString());
 			}
 
 		} catch (Exception e) {
@@ -80,4 +102,72 @@ public class TableUpdate {
 		return 0;
 	}
 
+	/**
+	 * Lookup the services associated with a system.
+	 * 
+	 * @param platformID
+	 *            the ID of the platform to which the required system is connected.
+	 * 
+	 * @param systemID
+	 *            the ID of the system for which the service list is required.
+	 * 
+	 * @return the service list encoded in JSON.
+	 * 
+	 * @throws SQLException
+	 */
+	private static StringBuilder query(String platformID, String systemID) throws SQLException {
+
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		StringBuilder services = new StringBuilder(",\"services\":");
+
+		try {
+
+			/* Get the database connection */
+			connection = DriverManager.getConnection("jdbc:default:connection");
+
+			/* Build the query */
+			String sql = "select * from data_feeds where platform_id=? and service_id=?";
+			statement = connection.prepareStatement(sql);
+
+			/* Set the parameter */
+			statement.setString(1, platformID);
+			statement.setString(2, systemID);
+			resultSet = statement.executeQuery();
+
+			/* Convert the result to JSON */
+
+			while (resultSet.next()) {
+
+				services.append("[{");
+				services.append("\"id\":\"" + resultSet.getString("ID") + "\"");
+				services.append("\"type\":\"" + resultSet.getString("TYPE_ID") + "\"");
+				services.append("\"mode\":\"" + resultSet.getString("DIRECTION") + "\"");
+				services.append('}');
+
+				if (!resultSet.isLast()) {
+					services.append(',');
+				}
+			}
+
+		} finally {
+
+			/* Tidy up */
+
+			if (resultSet != null) {
+				resultSet.close();
+			}
+
+			if (statement != null) {
+				statement.close();
+			}
+
+			if (connection != null) {
+				connection.close();
+			}
+		}
+
+		return services;
+	}
 }
