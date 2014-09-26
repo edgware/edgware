ef32fff832825bad341852f59243bea4d2b03c99
MagowanJames <magowan@uk.ibm.com>
2014-09-26 17:51:55 +0100

    Fix so discovery multicasts will restart after a network failure

--- fabric.fablets.autodiscovery/src/fabric/fablets/autodiscovery/AutoDiscoveryRequestFablet.java
+++ fabric.fablets.autodiscovery/src/fabric/fablets/autodiscovery/AutoDiscoveryRequestFablet.java
@@ -16,8 +16,10 @@ import java.net.MulticastSocket;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -77,6 +79,9 @@ public class AutoDiscoveryRequestFablet extends FabricBus implements IFabletPlug
 	 * there can be more than one discoveryRequestString with corresponding MulticastSocket
 	 */
 	private Map<String, MulticastSocket> requestToSocket = new HashMap<String, MulticastSocket>();
+	private Map<String, NodeIpMapping> requestToNodeIpMapping = new HashMap<String, NodeIpMapping>();
+	
+	private List<String> failedRequests = new ArrayList<String>();
 
 	/** Frequency with which to issue the AutoDiscovery broadcast. */
 	private int autoDiscoveryFrequency;
@@ -188,48 +193,63 @@ public class AutoDiscoveryRequestFablet extends FabricBus implements IFabletPlug
 		for (int i = 0; i < nodeIpMappings.length; i++) {
 
 			NodeIpMapping nodeIpMapping = nodeIpMappings[i];
-			InetAddress interfaceSendRequestAddr = null;
 			String nodePort = Integer.toString(nodeIpMapping.getPort());
 			String nodeInterface = nodeIpMapping.getNodeInterface();
 			String nodeAffiliation = localNode.getAffiliation();
 			String request = "N!:" + nodeType + ":" + myNodeName + ":" + nodeInterface + ":" + nodePort + ":"
 					+ nodeAffiliation;
+			openRequestSocket(request, nodeIpMapping);
+			
+		}
+	}
+	
+	/**
+	 * Opens the multicast socket
+	 * 
+	 * @param request message to be sent
+	 * @param nodeIpMapping ipmapping information for opening multicast socket
+	 */
+	private void openRequestSocket(String request, NodeIpMapping nodeIpMapping) {
+		InetAddress interfaceSendRequestAddr = null;
 
-			try {
+		try {
 
-				interfaceSendRequestAddr = InetAddress.getByName(nodeIpMapping.getIpAddress());
-				MulticastSocket udpSendRequestSocket;
-				udpSendRequestSocket = new MulticastSocket();
-				udpSendRequestSocket.setTimeToLive(broadcastMessageTTL);
-				udpSendRequestSocket.setNetworkInterface(NetworkInterface.getByInetAddress(interfaceSendRequestAddr));
-				udpSendRequestSocket.joinGroup(multicastGroupAddress);
+			interfaceSendRequestAddr = InetAddress.getByName(nodeIpMapping.getIpAddress());
+			MulticastSocket udpSendRequestSocket;
+			udpSendRequestSocket = new MulticastSocket();
+			udpSendRequestSocket.setTimeToLive(broadcastMessageTTL);
+			udpSendRequestSocket.setNetworkInterface(NetworkInterface.getByInetAddress(interfaceSendRequestAddr));
+			udpSendRequestSocket.joinGroup(multicastGroupAddress);
 
-				logger.log(Level.INFO, "Multicast send socket created for: {0} | {1} - {2} | {3}", new Object[] {
-						multicastGroupAddress.toString(),
-						NetworkInterface.getByInetAddress(interfaceSendRequestAddr).toString(),
-						udpSendRequestSocket.getInterface().toString(),
-						udpSendRequestSocket.getLocalAddress().toString()});
+			logger.log(Level.INFO, "Multicast send socket created for: {0} | {1} - {2} | {3}", new Object[] {
+					multicastGroupAddress.toString(),
+					NetworkInterface.getByInetAddress(interfaceSendRequestAddr).toString(),
+					udpSendRequestSocket.getInterface().toString(),
+					udpSendRequestSocket.getLocalAddress().toString()});
 
-				requestToSocket.put(request, udpSendRequestSocket);
+			requestToSocket.put(request, udpSendRequestSocket);
+			requestToNodeIpMapping.put(request, nodeIpMapping);
 
-			} catch (UnknownHostException e) {
-				logger.log(Level.INFO,
-						"Couldn't get an InetAddress for the IP {0} for interface {1} on node {2} : {3}", new Object[] {
-								nodeIpMapping.getIpAddress(), nodeIpMapping.getNodeInterface(),
-								nodeIpMapping.getNodeId(), LogUtil.stackTrace(e)});
-			} catch (SocketException e) {
-				logger.log(
-						Level.WARNING,
-						"Could not create broadcast/multicast socket to send to group \"{0}\" from interface \"{1}\": {2}",
-						new Object[] {multicastGroupAddress.toString(), interfaceSendRequestAddr.toString(),
-								LogUtil.stackTrace(e)});
-			} catch (IOException e) {
-				logger.log(
-						Level.WARNING,
-						"Could not create broadcast/multicast socket to send to group \"{0}\" from interface \"{1}\": {2}",
-						new Object[] {multicastGroupAddress.toString(), interfaceSendRequestAddr.toString(),
-								LogUtil.stackTrace(e)});
-			}
+		} catch (UnknownHostException e) {
+			logger.log(Level.INFO,
+					"Couldn't get an InetAddress for the IP {0} for interface {1} on node {2} : {3}", new Object[] {
+					nodeIpMapping.getIpAddress(), nodeIpMapping.getNodeInterface(),
+					nodeIpMapping.getNodeId(), LogUtil.stackTrace(e)});
+			failedRequests.add(request);
+		} catch (SocketException e) {
+			logger.log(
+					Level.WARNING,
+					"Could not create broadcast/multicast socket to send to group \"{0}\" from interface \"{1}\": {2}",
+					new Object[] {multicastGroupAddress.toString(), interfaceSendRequestAddr.toString(),
+							LogUtil.stackTrace(e)});
+			failedRequests.add(request);
+		} catch (IOException e) {
+			logger.log(
+					Level.WARNING,
+					"Could not create broadcast/multicast socket to send to group \"{0}\" from interface \"{1}\": {2}",
+					new Object[] {multicastGroupAddress.toString(), interfaceSendRequestAddr.toString(),
+							LogUtil.stackTrace(e)});
+			failedRequests.add(request);
 		}
 	}
 
@@ -289,39 +309,54 @@ public class AutoDiscoveryRequestFablet extends FabricBus implements IFabletPlug
 					timeTakenToBroadcast = System.currentTimeMillis();
 				}
 
-				try {
+				//Try to reopen sockets for any failed requests
+				List<String> oldFailedRequests = new ArrayList<String>();
+				oldFailedRequests.addAll(failedRequests);
+				failedRequests.clear();
+				for (Iterator<String> iter = oldFailedRequests.iterator(); iter
+						.hasNext();) {
+					String request = (String) iter.next();
+					NodeIpMapping nodeIpMapping = requestToNodeIpMapping.get(request);
+					openRequestSocket(request, nodeIpMapping);				
+				}
+				
+				
+				/* For each interface's MulticastSocket... */
+				for (Iterator<String> iterator = requestToSocket.keySet().iterator(); iterator.hasNext();) {
 
-					/* For each interface's MulticastSocket... */
-					for (Iterator<String> iterator = requestToSocket.keySet().iterator(); iterator.hasNext();) {
+					/* Send a multicast discovery message */
 
-						/* Send a multicast discovery message */
+					String request = (String) iterator.next();
+					MulticastSocket udpSendRequestSocket = requestToSocket.get(request);
+					if (requestOrdinal == Integer.MAX_VALUE) {
+						requestOrdinal = 0;
+					}
+					requestOrdinal++;
 
-						String request = (String) iterator.next();
-						MulticastSocket udpSendRequestSocket = requestToSocket.get(request);
-						if (requestOrdinal == Integer.MAX_VALUE) {
-							requestOrdinal = 0;
-						}
-						requestOrdinal++;
+					timeOfBroadcast = System.currentTimeMillis();
+					sendRequest = request + ":" + Integer.toString(requestOrdinal) + ":"
+							+ Long.toString(timeOfBroadcast);
+					byte[] rbuf = sendRequest.getBytes();
+					DatagramPacket p = new DatagramPacket(rbuf, rbuf.length, multicastGroupAddress, multicastPort);
 
-						timeOfBroadcast = System.currentTimeMillis();
-						sendRequest = request + ":" + Integer.toString(requestOrdinal) + ":"
-								+ Long.toString(timeOfBroadcast);
-						byte[] rbuf = sendRequest.getBytes();
-						DatagramPacket p = new DatagramPacket(rbuf, rbuf.length, multicastGroupAddress, multicastPort);
-
+					try {
 						udpSendRequestSocket.send(p);
 					}
+					catch (IOException e) {
+						/* Could not send packet */
+						logger.log(Level.WARNING, "Could not send broadcast/multicast packet to group \"{0}\": {1}",
+								new Object[] {multicastGroupAddress.getHostAddress(), e.getMessage()});
 
-				} catch (IOException e) {
+						/* Clean everything up and restart */
+						udpSendRequestSocket.close();
+						failedRequests.add(request);						
+					}
+				}
 
-					/* Could not send packet */
-					logger.log(Level.WARNING, "Could not send broadcast/multicast packet to group \"{0}\": {1}",
-							new Object[] {multicastGroupAddress.getHostAddress(), e.getMessage()});
-
-					/* Clean everything up and restart */
-					closeRequestSockets();
-					openRequestSockets();
-
+				for (Iterator<String> iter = failedRequests.iterator(); iter
+						.hasNext();) {
+					String request = (String) iter.next();
+					requestToSocket.remove(request);					
 				}
 
 				/* Wait before we send the next Broadcasts */
