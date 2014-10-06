5aa08519b0d51cd049d5bfb44cde95e89168fd51
Christopher Gibson <christopher.gibson@uk.ibm.com>
2014-10-06 17:12:24 +0100

    New Fabric excpetion base class added, along with an enumerated type to encode the casues of exceptions.

---
+++ fabric.lib/src/fabric/FabricException.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed Materials - Property of IBM
+ *  
+ * (C) Copyright IBM Corp. 2014
+ * 
+ * LICENSE: Eclipse Public License v1.0
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package fabric;
+
+
+/**
+ * Exception thrown when Fabric errors occur.
+ */
+public class FabricException extends Exception {
+
+	/** Copyright notice. */
+	public static final String copyrightNotice = "(C) Copyright IBM Corp. 2014";
+
+	/*
+	 * Class constants
+	 */
+
+	private static final long serialVersionUID = 6880534927302972779L;
+
+	/*
+	 * Class fields
+	 */
+
+	/** The cause of the exception */
+	private ReasonCode cause = ReasonCode.UNKNOWN;
+
+	/*
+	 * Class methods
+	 */
+
+	/**
+	 * Constructs a new instance.
+	 * 
+	 * @param cause
+	 *            the cause of the exception.
+	 */
+	public FabricException(ReasonCode cause, String message) {
+		super(message);
+		this.cause = cause;
+	}
+
+	/**
+	 * Answers the cause of this exception.
+	 * 
+	 * @return the cause.
+	 */
+	public ReasonCode getReason() {
+		return cause;
+	}
+}
