b5ae9b9773d19c419d7fcaf3730d67c711d68e05
MagowanJames <magowan@uk.ibm.com>
2014-10-30 23:13:21 +0000

    Add Json Command Line Interface. Useful for testing subscription capability from a console window

---
+++ fabric.admin/src/fabric/tools/json/JsonCLIResponseListener.java
@@ -0,0 +1,28 @@
+package fabric.tools.json;
+
+public class JsonCLIResponseListener extends Thread {
+	
+	JsonClient jsonClient = null;
+	long timeout = 100000; 
+	
+	public JsonCLIResponseListener(JsonClient jsonClient) {
+		this.jsonClient = jsonClient;
+	}
+	
+	@Override
+	public synchronized void run() {
+		while (true)
+		{
+			try {
+				String response = jsonClient.getResponse(timeout);
+				if (response != null ) {
+					System.out.println("RESPONSE : " + response);
+				}
+			} catch (InterruptedException e) {
+				
+			//Expected just loop again
+			}
+		}
+	}
+
+}
