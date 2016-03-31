eca9092a829117b3aa4205a07d4cb530df82c2ad
gibsoncr <gibsoncr@uk.ibm.com>
2016-03-31 23:40:21 +0100

    Updated header comments.

--- bundle.apache.derby/src/bundle/apache/derby/Placeholder.java
+++ bundle.apache.derby/src/bundle/apache/derby/Placeholder.java
@@ -1,8 +1,6 @@
 /*
- * Licensed Materials - Property of IBM
- *  
  * (C) Copyright IBM Corp. 2014
- * 
+ *
  * LICENSE: Eclipse Public License v1.0
  * http://www.eclipse.org/legal/epl-v10.html
  */
@@ -10,9 +8,8 @@
 package bundle.apache.derby;
 
 public class Placeholder {
-	
-	/** Copyright notice. */
-	public static final String copyrightNotice = "(C) Copyright IBM Corp. 2014";
 
+    /** Copyright notice. */
+    public static final String copyrightNotice = "(C) Copyright IBM Corp. 2014";
 
 }
--- bundle.jackson/src/bundle/jackson/Activator.java
+++ bundle.jackson/src/bundle/jackson/Activator.java
@@ -1,8 +1,6 @@
 /*
- * Licensed Materials - Property of IBM
- *  
  * (C) Copyright IBM Corp. 2014
- * 
+ *
  * LICENSE: Eclipse Public License v1.0
  * http://www.eclipse.org/legal/epl-v10.html
  */
@@ -14,30 +12,30 @@ import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
-	/** Copyright notice. */
-	public static final String copyrightNotice = "(C) Copyright IBM Corp. 2014";
+    /** Copyright notice. */
+    public static final String copyrightNotice = "(C) Copyright IBM Corp. 2014";
 
-	/*
-	 * Class methods
-	 */
+    /*
+     * Class methods
+     */
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	@Override
-	public void start(BundleContext context) throws Exception {
+    /*
+     * (non-Javadoc)
+     * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void start(BundleContext context) throws Exception {
 
-		System.out.println("Jackson bundle started.");
-	}
+        System.out.println("Jackson bundle started.");
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	@Override
-	public void stop(BundleContext context) throws Exception {
+    /*
+     * (non-Javadoc)
+     * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void stop(BundleContext context) throws Exception {
 
-		System.out.println("Jackson bundle stopped.");
-	}
+        System.out.println("Jackson bundle stopped.");
+    }
 }
