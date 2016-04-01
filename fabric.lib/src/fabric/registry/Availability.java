5bdfedf94d7ddcef6d3b3df5f8f6a410d0ee39f6
gibsoncr <gibsoncr@uk.ibm.com>
2016-04-01 11:49:29 +0100

    New QueryScope enumerated type to replace true/false flag indicating local versus distributed Registry queries.

---
+++ fabric.lib/src/fabric/registry/Availability.java
@@ -0,0 +1,18 @@
+/*
+ * (C) Copyright IBM Corp. 2016
+ *
+ * LICENSE: Eclipse Public License v1.0
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package fabric.registry;
+
+/**
+ * Enumberated type indicating if Registry queries should be performed LOCAL or DISTRIBUTED.
+ *
+ */
+public enum Availability {
+
+    /** (C) Copyright IBM Corp. 2016 */
+    LOCAL, DISTRIBUTED
+}
