92af9902b73d2efeb4c8bee63975af900e297eff
gibsoncr <gibsoncr@uk.ibm.com>
2016-04-01 11:38:22 +0100

    Refactoring for new QueryScope enumerated type to replace true/false flag indicated local versus distributed Registry queries.

--- fabric.lib/src/fabric/bus/feeds/impl/SubscriptionCollection.java
+++ fabric.lib/src/fabric/bus/feeds/impl/SubscriptionCollection.java
@@ -1,8 +1,6 @@
 /*
- * Licensed Materials - Property of IBM
- *  
  * (C) Copyright IBM Corp. 2012
- * 
+ *
  * LICENSE: Eclipse Public License v1.0
  * http://www.eclipse.org/legal/epl-v10.html
  */
@@ -24,6 +22,7 @@ import fabric.client.FabricClient;
 import fabric.registry.FabricRegistry;
 import fabric.registry.FeedRoutes;
 import fabric.registry.RegistryObject;
+import fabric.registry.QueryScope;
 import fabric.registry.exception.FactoryCreationException;
 import fabric.registry.exception.PersistenceException;
 import fabric.registry.impl.FeedRoutesFactoryImpl;
@@ -33,159 +32,159 @@ import fabric.registry.impl.FeedRoutesFactoryImpl;
  */
 public abstract class SubscriptionCollection implements ISubscriptionCollection, ISubscriptionCallback {
 
-	/** Copyright notice. */
-	public static final String copyrightNotice = "(C) Copyright IBM Corp. 2012";
+    /** Copyright notice. */
+    public static final String copyrightNotice = "(C) Copyright IBM Corp. 2012";
 
-	protected Logger logger;
+    protected Logger logger;
 
-	/* Table of active subscriptions managed by this collection */
-	protected HashMap<TaskServiceDescriptor, ISubscription> subscriptions = null;
+    /* Table of active subscriptions managed by this collection */
+    protected HashMap<TaskServiceDescriptor, ISubscription> subscriptions = null;
 
-	private ISubscriptionCallback callback = null;
+    private ISubscriptionCallback callback = null;
 
-	protected FabricClient fabricClient;
+    protected FabricClient fabricClient;
 
-	protected SubscriptionCollection(FabricClient fabricClient) {
+    protected SubscriptionCollection(FabricClient fabricClient) {
 
-		this.logger = Logger.getLogger("fabric.bus.feeds");
-		this.fabricClient = fabricClient;
-		subscriptions = new HashMap<TaskServiceDescriptor, ISubscription>();
-	}
+        this.logger = Logger.getLogger("fabric.bus.feeds");
+        this.fabricClient = fabricClient;
+        subscriptions = new HashMap<TaskServiceDescriptor, ISubscription>();
+    }
 
-	protected void subscribe(TaskServiceDescriptor taskServiceDescriptor, String[] route) throws Exception {
+    protected void subscribe(TaskServiceDescriptor taskServiceDescriptor, String[] route) throws Exception {
 
-		// Check we are not already subscribed to this feed
-		if (subscriptions.containsKey(taskServiceDescriptor)) {
-			throw new IllegalStateException("Collection already subscribed to " + taskServiceDescriptor);
-		}
+        // Check we are not already subscribed to this feed
+        if (subscriptions.containsKey(taskServiceDescriptor)) {
+            throw new IllegalStateException("Collection already subscribed to " + taskServiceDescriptor);
+        }
 
-		// Create the subscription object
-		ISubscription subscription = new Subscription(fabricClient);
+        // Create the subscription object
+        ISubscription subscription = new Subscription(fabricClient);
 
-		if (route != null) {
+        if (route != null) {
 
-			if (route.length == 0) {
-				throw new IllegalArgumentException("Route cannot be zero-length");
-			}
+            if (route.length == 0) {
+                throw new IllegalArgumentException("Route cannot be zero-length");
+            }
 
-			// Subscribe using the provided route
-			subscription.subscribe(taskServiceDescriptor, route, this);
+            // Subscribe using the provided route
+            subscription.subscribe(taskServiceDescriptor, route, this);
 
-		} else {
+        } else {
 
-			// Subscribe without providing a route
-			subscription.subscribe(taskServiceDescriptor, this);
+            // Subscribe without providing a route
+            subscription.subscribe(taskServiceDescriptor, this);
 
-		}
+        }
 
-		subscriptions.put(taskServiceDescriptor, subscription);
-	}
+        subscriptions.put(taskServiceDescriptor, subscription);
+    }
 
-	protected void setCallback(ISubscriptionCallback callback) {
+    protected void setCallback(ISubscriptionCallback callback) {
 
-		this.callback = callback;
-	}
+        this.callback = callback;
+    }
 
-	@Override
-	public ISubscription[] subscriptions() {
+    @Override
+    public ISubscription[] subscriptions() {
 
-		return subscriptions.values().toArray(new ISubscription[] {});
-	}
+        return subscriptions.values().toArray(new ISubscription[] {});
+    }
 
-	/**
-	 * @see ISubscriptionCollection#unsubscribe()
-	 */
-	@Override
-	public void unsubscribe() throws Exception {
+    /**
+     * @see ISubscriptionCollection#unsubscribe()
+     */
+    @Override
+    public void unsubscribe() throws Exception {
 
-		Iterator<TaskServiceDescriptor> it = subscriptions.keySet().iterator();
-		while (it.hasNext()) {
-			TaskServiceDescriptor feed = it.next();
-			ISubscription sub = subscriptions.get(feed);
-			sub.unsubscribe();
-			it.remove();
-		}
-	}
+        Iterator<TaskServiceDescriptor> it = subscriptions.keySet().iterator();
+        while (it.hasNext()) {
+            TaskServiceDescriptor feed = it.next();
+            ISubscription sub = subscriptions.get(feed);
+            sub.unsubscribe();
+            it.remove();
+        }
+    }
 
-	/**
-	 * Get all the FeedRoute objects represented by the specified feed pattern.
-	 * 
-	 * @param feedPattern
-	 * @return
-	 * @throws PersistenceException
-	 * @throws FactoryCreationException
-	 */
-	protected FeedRoutes[] getFeedRoutes(TaskServiceDescriptor feedPattern) throws PersistenceException,
-			FactoryCreationException {
+    /**
+     * Get all the FeedRoute objects represented by the specified feed pattern.
+     * 
+     * @param feedPattern
+     * @return
+     * @throws PersistenceException
+     * @throws FactoryCreationException
+     */
+    protected FeedRoutes[] getFeedRoutes(TaskServiceDescriptor feedPattern) throws PersistenceException,
+            FactoryCreationException {
 
-		// Query not restricted to just local
-		RegistryObject[] objects = FabricRegistry.runQuery(FeedRoutesFactoryImpl.getRouteQuery(feedPattern.task(),
-				feedPattern.platform(), feedPattern.system(), feedPattern.service(), fabricClient.homeNode()),
-				FeedRoutesFactoryImpl.class, false);
+        // Query not restricted to just local
+        RegistryObject[] objects = FabricRegistry.runQuery(FeedRoutesFactoryImpl.getRouteQuery(feedPattern.task(),
+                feedPattern.platform(), feedPattern.system(), feedPattern.service(), fabricClient.homeNode()),
+                FeedRoutesFactoryImpl.class, QueryScope.DISTRIBUTED);
 
-		/* Get the matching list of feeds */
-		FeedRoutes[] matchingFeeds = null;
+        /* Get the matching list of feeds */
+        FeedRoutes[] matchingFeeds = null;
 
-		/* If matching feeds have been found... */
-		if (objects != null && objects.length > 0) {
+        /* If matching feeds have been found... */
+        if (objects != null && objects.length > 0) {
 
-			/* Cast the results to FeedRoutes objects */
+            /* Cast the results to FeedRoutes objects */
 
-			matchingFeeds = new FeedRoutes[objects.length];
+            matchingFeeds = new FeedRoutes[objects.length];
 
-			/* For each feed... */
-			for (int f = 0; f < objects.length; f++) {
+            /* For each feed... */
+            for (int f = 0; f < objects.length; f++) {
 
-				matchingFeeds[f] = (FeedRoutes) objects[f];
-			}
+                matchingFeeds[f] = (FeedRoutes) objects[f];
+            }
 
-		} else {
+        } else {
 
-			logger.log(Level.FINE, "No matching feeds: {0} asset details found", (objects == null) ? "null"
-					: objects.length);
+            logger.log(Level.FINE, "No matching feeds: {0} asset details found", (objects == null) ? "null"
+                    : objects.length);
 
-			matchingFeeds = new FeedRoutes[0];
+            matchingFeeds = new FeedRoutes[0];
 
-		}
+        }
 
-		return matchingFeeds;
-	}
+        return matchingFeeds;
+    }
 
-	@Override
-	public void cancelSubscriptionCallback() {
+    @Override
+    public void cancelSubscriptionCallback() {
 
-		if (this.callback != null) {
-			this.callback.cancelSubscriptionCallback();
-		}
-	}
+        if (this.callback != null) {
+            this.callback.cancelSubscriptionCallback();
+        }
+    }
 
-	@Override
-	public void handleSubscriptionEvent(ISubscription subscription, int event, IServiceMessage message) {
+    @Override
+    public void handleSubscriptionEvent(ISubscription subscription, int event, IServiceMessage message) {
 
-		/* Remove the subscription from our table of managed subscriptions */
-		if (event == IServiceMessage.EVENT_SUBSCRIPTION_LOST) {
-			subscriptions.remove(subscription.feed());
-		}
+        /* Remove the subscription from our table of managed subscriptions */
+        if (event == IServiceMessage.EVENT_SUBSCRIPTION_LOST) {
+            subscriptions.remove(subscription.feed());
+        }
 
-		if (this.callback != null) {
-			this.callback.handleSubscriptionEvent(subscription, event, message);
-		}
-	}
+        if (this.callback != null) {
+            this.callback.handleSubscriptionEvent(subscription, event, message);
+        }
+    }
 
-	@Override
-	public void handleSubscriptionMessage(IFeedMessage message) {
+    @Override
+    public void handleSubscriptionMessage(IFeedMessage message) {
 
-		if (this.callback != null) {
-			this.callback.handleSubscriptionMessage(message);
-		}
-	}
+        if (this.callback != null) {
+            this.callback.handleSubscriptionMessage(message);
+        }
+    }
 
-	@Override
-	public void startSubscriptionCallback() {
+    @Override
+    public void startSubscriptionCallback() {
 
-		if (this.callback != null) {
-			this.callback.startSubscriptionCallback();
-		}
-	}
+        if (this.callback != null) {
+            this.callback.startSubscriptionCallback();
+        }
+    }
 
 }
