c35b7772dbb3a1d77bebf9a47ccf4b6e3d3a5ab0
gibsoncr <gibsoncr@uk.ibm.com>
2016-04-01 12:13:04 +0100

    1. Update header comments. 2. Shortened MQTT client ID. 3. Improvements to logging messages.

--- fabric.services.interfaces.json/src/fabric/services/jsonclient/HTTPAdapter.java
+++ fabric.services.interfaces.json/src/fabric/services/jsonclient/HTTPAdapter.java
@@ -1,8 +1,6 @@
 /*
- * Licensed Materials - Property of IBM
- *  
  * (C) Copyright IBM Corp. 2014
- * 
+ *
  * LICENSE: Eclipse Public License v1.0
  * http://www.eclipse.org/legal/epl-v10.html
  */
@@ -11,6 +9,7 @@ package fabric.services.jsonclient;
 
 import org.eclipse.jetty.websocket.api.Session;
 
+import fabric.core.io.mqtt.MqttConfig;
 import fabric.services.json.JSON;
 import fabric.services.jsonclient.utilities.AdapterConstants;
 
@@ -19,53 +18,56 @@ import fabric.services.jsonclient.utilities.AdapterConstants;
  */
 public class HTTPAdapter extends JSONAdapter {
 
-	/** Copyright notice. */
-	public static final String copyrightNotice = "(C) Copyright IBM Corp. 2014";
+    /** Copyright notice. */
+    public static final String copyrightNotice = "(C) Copyright IBM Corp. 2014";
 
-	/** The name of the class to act as a proxy for systems started by this adapter. */
-	private static final String HTTP_SYSTEM_ADAPTER = "fabric.services.systems.HTTPSystem";
+    /** The name of the class to act as a proxy for systems started by this adapter. */
+    private static final String HTTP_SYSTEM_ADAPTER = "fabric.services.systems.HTTPSystem";
 
-	/** Adapter ID */
-	public static String adapterId = "";
+    /** Adapter ID */
+    public static String adapterId = "";
 
-	/**
-	 * @see fabric.services.jsonclient.JSONAdapter#init()
-	 */
-	@Override
-	public void init() throws Exception {
-		super.init();
-	}
+    /**
+     * @see fabric.services.jsonclient.JSONAdapter#init()
+     */
+    @Override
+    public void init() throws Exception {
 
-	/**
-	 * @see fabric.services.jsonclient.JSONAdapter#adapterProxy()
-	 */
-	@Override
-	public String adapterProxy() {
-		return HTTP_SYSTEM_ADAPTER;
-	}
+        setAdapterUserID(MqttConfig.generateClient("HA"));
+        setAdapterPlatformID(MqttConfig.generateClient("HA"));
+        setAdapterPlatformType("HTTP_ADAPTER");
+        super.init();
+    }
 
-	/**
-	 * Method that handles the incoming JSON message from HTTP client. Used when you expect a response message from
-	 * Fabric for non-SQL items
-	 */
-	public JSON getJSONResponseMessage(JSON obj, Session session) {
-		/*
-		 * Correlation id is null so JSONAdapter can handle the case whereby no correlation id exists
-		 */
-		String correlationId = null;
-		JSON response = new JSON();
-		correlationId = obj.getString("correl");
-		boolean isSQLQuery = obj.getString(AdapterConstants.FIELD_SQL_KEY) != null
-				|| obj.getString(AdapterConstants.FIELD_SQL_TABLE) != null;
+    /**
+     * @see fabric.services.jsonclient.JSONAdapter#adapterProxy()
+     */
+    @Override
+    public String adapterProxy() {
+        return HTTP_SYSTEM_ADAPTER;
+    }
 
-		try {
-			response = handleAdapterMessage(obj, correlationId, session);
-			session.getRemote().sendString(response.toString());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    /**
+     * Method that handles the incoming JSON message from HTTP client. Used when you expect a response message from
+     * Fabric for non-SQL items
+     */
+    public JSON getJSONResponseMessage(JSON obj, Session session) {
+        /*
+         * Correlation id is null so JSONAdapter can handle the case whereby no correlation id exists
+         */
+        String correlationId = null;
+        JSON response = new JSON();
+        correlationId = obj.getString("correl");
+        boolean isSQLQuery = obj.getString(AdapterConstants.FIELD_SQL_KEY) != null
+                || obj.getString(AdapterConstants.FIELD_SQL_TABLE) != null;
 
-		return response;
-	}
+        try {
+            response = handleAdapterMessage(obj, correlationId, session);
+            session.getRemote().sendString(response.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        return response;
+    }
 }
--- fabric.services.interfaces.json/src/fabric/services/jsonclient/MQTTAdapter.java
+++ fabric.services.interfaces.json/src/fabric/services/jsonclient/MQTTAdapter.java
@@ -1,8 +1,6 @@
 /*
- * Licensed Materials - Property of IBM
- * 
  * (C) Copyright IBM Corp. 2014
- * 
+ *
  * LICENSE: Eclipse Public License v1.0
  * http://www.eclipse.org/legal/epl-v10.html
  */
@@ -21,7 +19,8 @@ import fabric.core.io.ICallback;
 import fabric.core.io.InputTopic;
 import fabric.core.io.Message;
 import fabric.core.io.OutputTopic;
-import fabric.core.logging.LogUtil;
+import fabric.core.io.mqtt.MqttConfig;
+import fabric.core.logging.FLog;
 import fabric.services.json.JSON;
 import fabric.services.jsonclient.utilities.AdapterConstants;
 import fabric.services.jsonclient.utilities.AdapterStatus;
@@ -31,267 +30,274 @@ import fabric.services.jsonclient.utilities.AdapterStatus;
  */
 public class MQTTAdapter extends JSONAdapter implements IFabricShutdownHookAction, ICallback {
 
-	/** Copyright notice. */
-	public static final String copyrightNotice = "(C) Copyright IBM Corp. 2014";
+    /** Copyright notice. */
+    public static final String copyrightNotice = "(C) Copyright IBM Corp. 2014";
 
-	/*
-	 * Class constants
-	 */
+    /*
+     * Class constants
+     */
 
-	/**
-	 * The name of the class to act as a proxy for systems started by this adapter.
-	 */
-	private static final String MQTT_SYSTEM_ADAPTER = "fabric.services.systems.MQTTSystem";
+    /**
+     * The name of the class to act as a proxy for systems started by this adapter.
+     */
+    private static final String MQTT_SYSTEM_ADAPTER = "fabric.services.systems.MQTTSystem";
 
-	/*
-	 * Class static fields
-	 */
+    /*
+     * Class static fields
+     */
 
-	/** The shutdown hook for this service. */
-	private static FabricShutdownHook shutdownHook = null;
+    /** The shutdown hook for this service. */
+    private static FabricShutdownHook shutdownHook = null;
 
-	/*
-	 * Class fields
-	 */
+    /*
+     * Class fields
+     */
 
-	/** The topic upon which commands from MQTT adapter clients are received. */
-	private InputTopic listenTopic = null;
+    /** The topic upon which commands from MQTT adapter clients are received. */
+    private InputTopic listenTopic = null;
 
-	/**
-	 * The Fabric channel through which commands from MQTT adapter clients are received.
-	 */
-	private SharedChannel listenChannel = null;
+    /**
+     * The Fabric channel through which commands from MQTT adapter clients are received.
+     */
+    private SharedChannel listenChannel = null;
 
-	/** The topic upon which responses to clients are sent. */
-	private OutputTopic sendTopic = null;
+    /** The topic upon which responses to clients are sent. */
+    private OutputTopic sendTopic = null;
 
-	/** The Fabric channel through which responses to clients are sent. */
-	private SharedChannel sendChannel = null;
+    /** The Fabric channel through which responses to clients are sent. */
+    private SharedChannel sendChannel = null;
 
-	/*
-	 * Static class initialization
-	 */
+    /*
+     * Static class initialization
+     */
 
-	static {
+    static {
 
-		/* Register the shutdown hook for this JVM */
-		shutdownHook = new FabricShutdownHook("fabric.services.jsonclient");
-		Runtime.getRuntime().addShutdownHook(shutdownHook);
+        /* Register the shutdown hook for this JVM */
+        shutdownHook = new FabricShutdownHook("fabric.services.jsonclient");
+        Runtime.getRuntime().addShutdownHook(shutdownHook);
 
-	}
+    }
 
-	/*
-	 * Class methods
-	 */
+    /*
+     * Class methods
+     */
 
-	public MQTTAdapter() {
+    public MQTTAdapter() {
 
-		this(Logger.getLogger("fabric.services.jsonclient"));
-	}
+        this(Logger.getLogger("fabric.services.jsonclient"));
+    }
 
-	public MQTTAdapter(Logger logger) {
+    public MQTTAdapter(Logger logger) {
 
-		this.logger = logger;
-	}
+        this.logger = logger;
+    }
 
-	/**
-	 * Main entry point.
-	 * 
-	 * @param cla
-	 *            the command line arguments.
-	 */
-	public static void main(final String[] cla) {
+    /**
+     * Main entry point.
+     *
+     * @param cla
+     *            the command line arguments.
+     */
+    public static void main(final String[] cla) {
 
-		try {
+        try {
 
-			MQTTAdapter adapter = new MQTTAdapter();
+            MQTTAdapter adapter = new MQTTAdapter();
 
-			/* Register this instance with the shutdown hook */
-			shutdownHook.addAction(adapter);
+            /* Register this instance with the shutdown hook */
+            shutdownHook.addAction(adapter);
 
-			adapter.init();
+            adapter.init();
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	/**
-	 * @see fabric.services.jsonclient.JSONAdapter#init()
-	 */
-	@Override
-	public void init() throws Exception {
+    /**
+     * @see fabric.services.jsonclient.JSONAdapter#init()
+     */
+    @Override
+    public void init() throws Exception {
 
-		super.init();
+        setAdapterUserID(MqttConfig.generateClient("MA"));
+        setAdapterPlatformID(MqttConfig.generateClient("MA"));
+        setAdapterPlatformType("HTTP_ADAPTER");
+        super.init();
 
-		/* Open the command channels */
-		openChannels();
+        /* Open the command channels */
+        openChannels();
 
-	}
-
-	/**
-	 * Open the Fabric channels for inbound and outbound adapter messages.
-	 * 
-	 * @throws IOException
-	 * @throws UnsupportedOperationException
-	 */
-	private void openChannels() throws IOException, UnsupportedOperationException {
-
-		/* Open the channel used for inbound MQTT messages */
-
-		listenTopic = new InputTopic(config("fabric.adapters.mqtt.intopic", null, homeNode()) + "/+");
-		listenChannel = homeNodeEndPoint().openInputChannel(listenTopic, this);
-
-		String inMessage = String.format("MQTT adapter: listening on topic \"%s\"", listenTopic);
-		System.out.println(inMessage);
-		logger.info(inMessage);
-
-		/* Open the channel used for outbound MQTT messages */
-
-		sendTopic = new OutputTopic(config("fabric.adapters.mqtt.outtopic", null, homeNode()));
-		sendChannel = homeNodeEndPoint().openOutputChannel(sendTopic);
-
-		String outMessage = String.format("MQTT adapter: sending to topic \"%s\"", sendTopic);
-		System.out.println(outMessage);
-		logger.info(outMessage);
-	}
-
-	/**
-	 * @see fabric.services.jsonclient.JSONAdapter#adapterProxy()
-	 */
-	@Override
-	public String adapterProxy() {
-
-		return MQTT_SYSTEM_ADAPTER;
-	}
-
-	/**
-	 * Invoked when this callback is being initialised.
-	 * 
-	 * @see fabric.core.io.ICallback#startCallback(java.lang.Object)
-	 */
-	@Override
-	public void startCallback(final Object arg1) {
-
-		/* No initialization required. */
-
-	}
-
-	/**
-	 * Invoked when an adapter message arrives.
-	 * 
-	 * @param message
-	 *            the new adapter message.
-	 * 
-	 * @see fabric.core.io.ICallback#handleMessage(fabric.core.io.Message)
-	 */
-	@Override
-	public synchronized void handleMessage(final Message message) {
-
-		JSON response = null;
-		String correlationID = null;
-
-		/* Get the ID of the MQTT adapter client */
-		String[] topicParts = ((String) message.topic).split("/");
-		String adapterClientID = topicParts[topicParts.length - 1];
-		logger.log(Level.FINEST, "Client ID: {0}", adapterClientID);
-
-		/* Get the JSON payload */
-		String opString = new String(message.data);
-
-		/* Convert the message to a JSON object */
-		try {
-
-			logger.log(Level.FINEST, "Handling JSON message:\n{0}", opString);
-			JSON op = new JSON(opString);
-
-			/* Get the message correlation ID from the message (if any) */
-			correlationID = op.getString(AdapterConstants.FIELD_CORRELATION_ID);
-			logger.log(Level.FINEST, "Correlation ID: {0}", correlationID);
-
-			/* Handle the message */
-			response = handleAdapterMessage(op, correlationID, adapterClientID);
-
-		} catch (Exception e) {
-
-			logger.log(Level.INFO, "Failed to parse JSON string:\n{0}");
-			AdapterStatus status = new AdapterStatus(AdapterConstants.ERROR_PARSE, AdapterConstants.OP_CODE_NONE,
-					AdapterConstants.ARTICLE_JSON, AdapterConstants.STATUS_MSG_BAD_JSON + ": " + e.getMessage(),
-					correlationID);
-			response = status.toJsonObject();
-
-		}
-
-		/* If a response is required by the client... */
-		if (response != null) {
-			try {
-				logger.log(Level.FINEST, "Response to client:\n{0}", response.toString());
-				sendChannel.write(response.toString().getBytes(), new OutputTopic(sendTopic + "/" + adapterClientID));
-			} catch (Exception e) {
-				logger.log(Level.WARNING, "Failed to send response to client \"{0}\" (correlation ID \"{1}\"): {2}",
-						new Object[] {adapterClientID, correlationID, LogUtil.stackTrace(e)});
-			}
-		}
-	}
-
-	/**
-	 * Invoked when this callback is being cancelled.
-	 * 
-	 * @see fabric.core.io.ICallback#cancelCallback(java.lang.Object)
-	 */
-	@Override
-	public void cancelCallback(final Object arg1) {
-
-		/* No cleanup required */
-	}
-
-	/**
-	 * @see fabric.IFabricShutdownHookAction#shutdown()
-	 */
-	@Override
-	public void shutdown() {
-
-		super.stop();
-	}
-
-	/**
-	 * @see fabric.services.jsonclient.JSONAdapter#endPointConnected(fabric.core.io.EndPoint)
-	 */
-	@Override
-	public void endPointConnected(EndPoint ep) {
-
-		super.endPointConnected(ep);
-
-	}
-
-	/**
-	 * @see fabric.services.jsonclient.JSONAdapter#endPointDisconnected(fabric.core.io.EndPoint)
-	 */
-	@Override
-	public void endPointDisconnected(EndPoint ep) {
-
-		super.endPointDisconnected(ep);
-
-	}
-
-	/**
-	 * @see fabric.services.jsonclient.JSONAdapter#endPointReconnected(fabric.core.io.EndPoint)
-	 */
-	@Override
-	public void endPointReconnected(EndPoint ep) {
-
-		super.endPointReconnected(ep);
-
-	}
-
-	/**
-	 * @see fabric.services.jsonclient.JSONAdapter#endPointClosed(fabric.core.io.EndPoint)
-	 */
-	@Override
-	public void endPointClosed(EndPoint ep) {
-
-		super.endPointClosed(ep);
-
-	}
+    }
+
+    /**
+     * Open the Fabric channels for inbound and outbound adapter messages.
+     *
+     * @throws IOException
+     * @throws UnsupportedOperationException
+     */
+    private void openChannels() throws IOException, UnsupportedOperationException {
+
+        /* Open the channel used for inbound MQTT messages */
+
+        listenTopic = new InputTopic(config("fabric.adapters.mqtt.intopic", null, homeNode()) + "/+");
+        listenChannel = homeNodeEndPoint().openInputChannel(listenTopic, this);
+
+        String inMessage = String.format("MQTT adapter: listening on topic [%s]", listenTopic);
+        System.out.println(inMessage);
+        logger.info(inMessage);
+
+        /* Open the channel used for outbound MQTT messages */
+
+        sendTopic = new OutputTopic(config("fabric.adapters.mqtt.outtopic", null, homeNode()));
+        sendChannel = homeNodeEndPoint().openOutputChannel(sendTopic);
+
+        String outMessage = String.format("MQTT adapter: sending to topic [%s]", sendTopic);
+        System.out.println(outMessage);
+        logger.info(outMessage);
+    }
+
+    /**
+     * @see fabric.services.jsonclient.JSONAdapter#adapterProxy()
+     */
+    @Override
+    public String adapterProxy() {
+
+        return MQTT_SYSTEM_ADAPTER;
+    }
+
+    /**
+     * Invoked when this callback is being initialised.
+     *
+     * @see fabric.core.io.ICallback#startCallback(java.lang.Object)
+     */
+    @Override
+    public void startCallback(final Object arg1) {
+
+        /* No initialization required. */
+
+    }
+
+    /**
+     * Invoked when an adapter message arrives.
+     *
+     * @param message
+     *            the new adapter message.
+     *
+     * @see fabric.core.io.ICallback#handleMessage(fabric.core.io.Message)
+     */
+    @Override
+    public synchronized void handleMessage(final Message message) {
+
+        FLog.enter(logger, Level.FINER, this, "handleMessage", message);
+
+        JSON response = null;
+        String correlationID = null;
+
+        /* Get the ID of the MQTT adapter client */
+        String[] topicParts = ((String) message.topic).split("/");
+        String adapterClientID = topicParts[topicParts.length - 1];
+        logger.log(Level.FINEST, "Client ID: {0}", adapterClientID);
+
+        /* Get the JSON payload */
+        String opString = new String(message.data);
+
+        /* Convert the message to a JSON object */
+        try {
+
+            logger.log(Level.FINEST, "Handling JSON message:\n{0}", opString);
+            JSON op = new JSON(opString);
+
+            /* Get the message correlation ID from the message (if any) */
+            correlationID = op.getString(AdapterConstants.FIELD_CORRELATION_ID);
+            logger.log(Level.FINEST, "Correlation ID: {0}", correlationID);
+
+            /* Handle the message */
+            response = handleAdapterMessage(op, correlationID, adapterClientID);
+
+        } catch (Exception e) {
+
+            logger.log(Level.INFO, "Failed to parse JSON string:\n{0}");
+            AdapterStatus status = new AdapterStatus(AdapterConstants.ERROR_PARSE, AdapterConstants.OP_CODE_NONE,
+                    AdapterConstants.ARTICLE_JSON, AdapterConstants.STATUS_MSG_BAD_JSON + ": " + e.getMessage(),
+                    (correlationID == null) ? "Unknown" : correlationID, opString);
+            response = status.toJsonObject();
+
+        }
+
+        /* If a response is required by the client... */
+        if (response != null) {
+            try {
+                logger.log(Level.FINEST, "Response to client:\n{0}", response.toString());
+                sendChannel.write(response.toString().getBytes(), new OutputTopic(sendTopic + "/" + adapterClientID));
+            } catch (Exception e) {
+                logger.log(Level.WARNING, "Failed to send response to client \"{0}\" (correlation ID \"{1}\"): {2}",
+                        new Object[] {adapterClientID, correlationID, FLog.stackTrace(e)});
+            }
+        }
+
+        FLog.exit(logger, Level.FINER, this, "handleMessage", null);
+    }
+
+    /**
+     * Invoked when this callback is being cancelled.
+     *
+     * @see fabric.core.io.ICallback#cancelCallback(java.lang.Object)
+     */
+    @Override
+    public void cancelCallback(final Object arg1) {
+
+        /* No cleanup required */
+    }
+
+    /**
+     * @see fabric.IFabricShutdownHookAction#shutdown()
+     */
+    @Override
+    public void shutdown() {
+
+        super.stop();
+    }
+
+    /**
+     * @see fabric.services.jsonclient.JSONAdapter#endPointConnected(fabric.core.io.EndPoint)
+     */
+    @Override
+    public void endPointConnected(EndPoint ep) {
+
+        super.endPointConnected(ep);
+
+    }
+
+    /**
+     * @see fabric.services.jsonclient.JSONAdapter#endPointDisconnected(fabric.core.io.EndPoint)
+     */
+    @Override
+    public void endPointDisconnected(EndPoint ep) {
+
+        super.endPointDisconnected(ep);
+
+    }
+
+    /**
+     * @see fabric.services.jsonclient.JSONAdapter#endPointReconnected(fabric.core.io.EndPoint)
+     */
+    @Override
+    public void endPointReconnected(EndPoint ep) {
+
+        super.endPointReconnected(ep);
+
+    }
+
+    /**
+     * @see fabric.services.jsonclient.JSONAdapter#endPointClosed(fabric.core.io.EndPoint)
+     */
+    @Override
+    public void endPointClosed(EndPoint ep) {
+
+        super.endPointClosed(ep);
+
+    }
 }
