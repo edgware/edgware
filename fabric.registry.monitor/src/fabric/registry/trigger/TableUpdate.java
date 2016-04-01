144bf2a905e30f03487f660ca97f60c9a45660b5
gibsoncr <gibsoncr@uk.ibm.com>
2016-04-01 12:19:28 +0100

    1. Update header comments. 2. Improvements to logging messages. 3. Addition of additional availability inforamation to neighbour triggers.

--- fabric.registry.monitor/src/fabric/registry/trigger/TableUpdate.java
+++ fabric.registry.monitor/src/fabric/registry/trigger/TableUpdate.java
@@ -1,8 +1,6 @@
 /*
- * Licensed Materials - Property of IBM
- *  
  * (C) Copyright IBM Corp. 2009, 2012
- * 
+ *
  * LICENSE: Eclipse Public License v1.0
  * http://www.eclipse.org/legal/epl-v10.html
  */
@@ -16,276 +14,285 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Logger;
 
-import fabric.core.logging.LogUtil;
+import fabric.core.logging.FLog;
 
 /**
  * Simple Fabric Service bound as a JavaUDF in the Registry. Used to publish Registry update events (INSERT, DELETE,
  * UPDATE) to the Fabric.
- * 
+ *
  */
 public class TableUpdate {
 
-	/** Copyright notice. */
-	public static final String copyrightNotice = "(C) Copyright IBM Corp. 2009, 2012";
+    /** Copyright notice. */
+    public static final String copyrightNotice = "(C) Copyright IBM Corp. 2009, 2012";
 
-	/*
-	 * Class static fields
-	 */
+    /*
+     * Class static fields
+     */
 
-	private static final Logger logger = Logger.getLogger("fabric.registry.trigger");
+    private static final Logger logger = Logger.getLogger("fabric.registry.trigger");
 
-	private static ResourceManager resourceManager = null;
+    private static ResourceManager resourceManager = null;
 
-	/*
-	 * Class methods
-	 */
+    /*
+     * Class methods
+     */
 
-	/**
-	 * Publish event data to the Fabric.
-	 * 
-	 * @param tableName
-	 *            the name of the table that was updated.
-	 * 
-	 * @param key
-	 *            the name of the primary key column.
-	 * 
-	 * @param id
-	 *            primary key of the row that was modified.
-	 * 
-	 * @param action
-	 *            the type of modification.
-	 * 
-	 * @return code always 0.
-	 */
-	public synchronized static int entryModified(String tableName, String key, String id, String action)
-			throws Exception {
+    /**
+     * Publish event data to the Fabric.
+     *
+     * @param tableName
+     *            the name of the table that was updated.
+     *
+     * @param key
+     *            the name of the primary key column.
+     *
+     * @param id
+     *            primary key of the row that was modified.
+     *
+     * @param action
+     *            the type of modification.
+     *
+     * @return code always 0.
+     */
+    public synchronized static int entryModified(String tableName, String key, String id, String action)
+            throws Exception {
 
-		try {
+        try {
 
-			if (resourceManager == null) {
-				resourceManager = ResourceManager.getInstance();
-			}
+            if (resourceManager == null) {
+                resourceManager = ResourceManager.getInstance();
+            }
 
-			StringBuilder notification = new StringBuilder("{");
-			StringBuilder notificationDetail = new StringBuilder();
-			tableName = (tableName != null) ? tableName.toUpperCase() : null;
-			action = (action != null) ? action.toUpperCase() : null;
+            StringBuilder notification = new StringBuilder("{");
+            StringBuilder notificationDetail = new StringBuilder();
+            tableName = (tableName != null) ? tableName.toUpperCase() : null;
+            action = (action != null) ? action.toUpperCase() : null;
+            String availability = null;
 
-			if (resourceManager.isReportable(tableName)) {
+            if (resourceManager.isReportable(tableName)) {
 
-				String[] idParts = null;
+                String[] idParts = null;
 
-				switch (tableName.toUpperCase()) {
+                switch (tableName.toUpperCase()) {
 
-				case "SYSTEMS":
+                    case "SYSTEMS":
 
-					/*
-					 * Determine the type of the system, and its name; the string is encoded as "id:type", where id is a
-					 * "/" separated string
-					 */
-					idParts = id.split(":");
-					String type = (idParts.length > 1) ? idParts[1] : "";
-					String[] keyParts = idParts[0].split("/");
+                        /*
+                         * Determine the type of the system, and its name; the string is encoded as "id:type", where id
+                         * is a "/" separated string
+                         */
+                        idParts = id.split(":");
+                        String type = (idParts.length > 1) ? idParts[1] : "";
+                        availability = (idParts.length > 2) ? idParts[2] : "";
+                        String[] keyParts = idParts[0].split("/");
 
-					if (action != null && (action.equals("UPDATE") || action.equals("INSERT"))) {
-						/* Lookup and record the services associated with this system */
-						notificationDetail = servicesForSystem(keyParts[0], keyParts[1], type);
-					}
+                        if (action != null && (action.equals("UPDATE") || action.equals("INSERT"))) {
+                            /* Lookup and record the services associated with this system */
+                            notificationDetail = servicesForSystem(keyParts[0], keyParts[1], type);
+                        }
 
-					break;
+                        break;
 
-				case "NODE_NEIGHBOURS":
+                    case "NODE_NEIGHBOURS":
 
-					/* Determine the ID of the neighbour; the string is encoded as "node_id/neighbour_id" */
-					idParts = id.split("/");
-					String nodeID = (idParts.length > 0) ? idParts[0] : "";
-					String neighbourID = (idParts.length > 0) ? idParts[1] : "";
+                        /*
+                         * Determine the ID of the neighbour; the string is encoded as
+                         * "node_id/neighbour_id:availability"
+                         */
+                        idParts = id.split("/");
+                        String nodeID = (idParts.length > 0) ? idParts[0] : "";
+                        String neighbour = (idParts.length > 1) ? idParts[1] : "";
+                        String[] neighbourParts = neighbour.split(":");
+                        String neighbourID = (neighbourParts.length > 0) ? neighbourParts[0] : "";
+                        availability = (neighbourParts.length > 1) ? neighbourParts[1] : "";
 
-					if (action != null && (action.equals("UPDATE") || action.equals("INSERT"))) {
-						notificationDetail = neighbourAvailability(nodeID, neighbourID);
-					}
+                        if (action != null && (action.equals("UPDATE") || action.equals("INSERT"))) {
+                            notificationDetail = neighbourAvailability(nodeID, neighbourID);
+                        }
 
-				}
+                }
 
-				/* Build the notification message (a JSON object) */
-				notification.append(String.format(
-						"\"table\":\"%s\",\"key\":\"%s\",\"id\":\"%s\",\"action\":\"%s\",\"timestamp\":%d", tableName,
-						key, id, action, System.currentTimeMillis()));
+                /* Build the notification message (a JSON object) */
+                notification
+                        .append(String
+                                .format("\"table\":\"%s\",\"key\":\"%s\",\"id\":\"%s\",\"action\":\"%s\",\"availability\":\"%s\",\"timestamp\":%d",
+                                        tableName, key, id, action, availability, System.currentTimeMillis()));
 
-				notification.append(notificationDetail);
-				notification.append('}');
+                notification.append(notificationDetail);
+                notification.append('}');
 
-				/* Publish to the Fabric */
-				resourceManager.sendRegistryUpdate(notification.toString());
-			}
+                /* Publish to the Fabric */
+                resourceManager.sendRegistryUpdate(notification.toString());
+            }
 
-		} catch (Exception e) {
+        } catch (Exception e) {
 
-			System.out.println("Trigger exception: " + e.getMessage());
-			e.printStackTrace();
+            System.out.println("Trigger exception: " + e.getMessage());
+            e.printStackTrace();
 
-		}
+        }
 
-		return 0;
-	}
+        return 0;
+    }
 
-	/**
-	 * Lookup the services associated with a system.
-	 * 
-	 * @param platformID
-	 *            the ID of the platform to which the required system is connected.
-	 * 
-	 * @param systemID
-	 *            the ID of the system to which the service is connected.
-	 * 
-	 * @param systemType
-	 *            the type of the system to which the service is connected.
-	 * 
-	 * @return the service list encoded in JSON.
-	 * 
-	 * @throws SQLException
-	 */
-	private static StringBuilder servicesForSystem(String platformID, String systemID, String systemType)
-			throws SQLException {
+    /**
+     * Lookup the services associated with a system.
+     *
+     * @param platformID
+     *            the ID of the platform to which the required system is connected.
+     *
+     * @param systemID
+     *            the ID of the system to which the service is connected.
+     *
+     * @param systemType
+     *            the type of the system to which the service is connected.
+     *
+     * @return the service list encoded in JSON.
+     *
+     * @throws SQLException
+     */
+    private static StringBuilder servicesForSystem(String platformID, String systemID, String systemType)
+            throws SQLException {
 
-		Connection connection = null;
-		Statement statement = null;
-		ResultSet resultSet = null;
-		StringBuilder services = new StringBuilder(",\"services\":[");
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        StringBuilder services = new StringBuilder(",\"services\":[");
 
-		try {
+        try {
 
-			/* Get the database connection */
-			connection = DriverManager.getConnection("jdbc:default:connection");
-//			connection = debugConnection;
+            /* Get the database connection */
+            connection = DriverManager.getConnection("jdbc:default:connection");
+            // connection = debugConnection;
 
-			/* Execute the query */
-			String sql = String.format("select * from data_feeds where platform_id='%s' and service_id='%s'",
-					platformID, systemID);
-			statement = connection.createStatement();
-			resultSet = statement.executeQuery(sql);
+            /* Execute the query */
+            String sql = String.format("select * from data_feeds where platform_id='%s' and service_id='%s'",
+                    platformID, systemID);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
 
-			/* Convert the result to JSON */
+            /* Convert the result to JSON */
 
-			while (resultSet.next()) {
+            while (resultSet.next()) {
 
-				if (services.lastIndexOf("}") == services.length() - 1) {
-					services.append(',');
-				}
+                if (services.lastIndexOf("}") == services.length() - 1) {
+                    services.append(',');
+                }
 
-				services.append(String.format("{\"id\":\"%s/%s%s/%s:%s:%s\"}", //
-						platformID, //
-						systemID, //
-						(systemType != null) ? ':' + systemType : "", //
-						resultSet.getString("ID"), //
-						resultSet.getString("TYPE_ID"), //
-						resultSet.getString("DIRECTION")));
-			}
+                services.append(String.format("{\"id\":\"%s/%s%s/%s:%s:%s\"}", //
+                        platformID, //
+                        systemID, //
+                        (systemType != null) ? ':' + systemType : "", //
+                        resultSet.getString("ID"), //
+                        resultSet.getString("TYPE_ID"), //
+                        resultSet.getString("DIRECTION")));
+            }
 
-			services.append(']');
+            services.append(']');
 
-		} catch (Exception e) {
+        } catch (Exception e) {
 
-			System.out.println("Query for system information failed: " + LogUtil.stackTrace(e));
+            System.out.println("Query for system information failed: " + FLog.stackTrace(e));
 
-		} finally {
+        } finally {
 
-			/* Tidy up */
+            /* Tidy up */
 
-			if (resultSet != null) {
-				resultSet.close();
-			}
+            if (resultSet != null) {
+                resultSet.close();
+            }
 
-			if (statement != null) {
-				statement.close();
-			}
+            if (statement != null) {
+                statement.close();
+            }
 
-			if (connection != null) {
-				connection.close();
-			}
-		}
+            if (connection != null) {
+                connection.close();
+            }
+        }
 
-		return services;
-	}
+        return services;
+    }
 
-	/**
-	 * Lookup the neighbour's availability.
-	 * 
-	 * @param nodeID
-	 *            the ID of the discovering node.
-	 * 
-	 * @param neighbourID
-	 *            the ID of the neighbour.
-	 * 
-	 * @throws SQLException
-	 */
-	private static StringBuilder neighbourAvailability(String nodeID, String neighbourID) throws SQLException {
+    /**
+     * Lookup the neighbour's availability.
+     *
+     * @param nodeID
+     *            the ID of the discovering node.
+     *
+     * @param neighbourID
+     *            the ID of the neighbour.
+     *
+     * @throws SQLException
+     */
+    private static StringBuilder neighbourAvailability(String nodeID, String neighbourID) throws SQLException {
 
-		Connection connection = null;
-		Statement statement = null;
-		ResultSet resultSet = null;
-		StringBuilder availabilityJSON = new StringBuilder();
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        StringBuilder availabilityJSON = new StringBuilder();
 
-		try {
+        try {
 
-			/* Get the database connection */
-			connection = DriverManager.getConnection("jdbc:default:connection");
-//			connection = debugConnection;
+            /* Get the database connection */
+            connection = DriverManager.getConnection("jdbc:default:connection");
+            // connection = debugConnection;
 
-			/* Execute the query */
-			String sql = String.format("select * from node_neighbours where node_id='%s' and neighbour_id='%s'",
-					nodeID, neighbourID);
-			statement = connection.createStatement();
-			resultSet = statement.executeQuery(sql);
+            /* Execute the query */
+            String sql = String.format("select * from node_neighbours where node_id='%s' and neighbour_id='%s'",
+                    nodeID, neighbourID);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
 
-			/* Convert the result to JSON */
-			resultSet.next();
-			String availability = resultSet.getString("AVAILABILITY");
-			availability = (availability != null) ? availability : "";
-			availabilityJSON.append(String.format(",\"availability\":\"%s\"", availability));
+            /* Convert the result to JSON */
+            resultSet.next();
+            String availability = resultSet.getString("AVAILABILITY");
+            availability = (availability != null) ? availability : "";
+            availabilityJSON.append(String.format(",\"availability\":\"%s\"", availability));
 
-		} catch (Exception e) {
+        } catch (Exception e) {
 
-			System.out.println("Query for node neighbour failed: " + LogUtil.stackTrace(e));
+            System.out.println("Query for node neighbour failed: " + FLog.stackTrace(e));
 
-		} finally {
+        } finally {
 
-			/* Tidy up */
+            /* Tidy up */
 
-			if (resultSet != null) {
-				resultSet.close();
-			}
+            if (resultSet != null) {
+                resultSet.close();
+            }
 
-			if (statement != null) {
-				statement.close();
-			}
+            if (statement != null) {
+                statement.close();
+            }
 
-			if (connection != null) {
-				connection.close();
-			}
-		}
+            if (connection != null) {
+                connection.close();
+            }
+        }
 
-		return availabilityJSON;
-	}
+        return availabilityJSON;
+    }
 
-//  Test harness for developing/debugging trigger code outside of Derby
-//	public static Connection debugConnection = null;
-//
-//	public static void main(String[] cla) {
-//
-//		try {
-//
-//			Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
-//			debugConnection = DriverManager
-//					.getConnection("jdbc:derby://localhost:6414/FABRIC;create=true;user=fabric;password=fabric");
-//
-//			TableUpdate.entryModified("SYSTEMS", "PLATFORM_ID/ID:TYPE_ID", "$fabric/$registry:$registry", "UPDATE");
-//			TableUpdate.entryModified("NODE_NEIGHBOURS", "NODE_ID/NEIGHBOUR_ID", "scott/alan", "UPDATE");
-//
-//		} catch (Exception e) {
-//
-//			e.printStackTrace();
-//
-//		}
-//	}
+    // Test harness for developing/debugging trigger code outside of Derby
+    // public static Connection debugConnection = null;
+    //
+    // public static void main(String[] cla) {
+    //
+    // try {
+    //
+    // Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+    // debugConnection = DriverManager
+    // .getConnection("jdbc:derby://localhost:6414/FABRIC;create=true;user=fabric;password=fabric");
+    //
+    // TableUpdate.entryModified("SYSTEMS", "PLATFORM_ID/ID:TYPE_ID", "$fabric/$registry:$registry", "UPDATE");
+    // TableUpdate.entryModified("NODE_NEIGHBOURS", "NODE_ID/NEIGHBOUR_ID", "scott/alan", "UPDATE");
+    //
+    // } catch (Exception e) {
+    //
+    // e.printStackTrace();
+    //
+    // }
+    // }
 }
