11061a08b78334c50e6e8c9dda00b2fec3be477e
gibsoncr <gibsoncr@uk.ibm.com>
2016-04-01 02:22:49 +0100

    1. Update header comments. 2. Re-format toString() output for improved clarity.

--- fabric.core/src/fabric/core/io/Message.java
+++ fabric.core/src/fabric/core/io/Message.java
@@ -1,8 +1,6 @@
 /*
- * Licensed Materials - Property of IBM
- * 
  * (C) Copyright IBM Corp. 2007, 2014
- * 
+ *
  * LICENSE: Eclipse Public License v1.0
  * http://www.eclipse.org/legal/epl-v10.html
  */
@@ -14,46 +12,46 @@ package fabric.core.io;
  */
 public class Message {
 
-	/** Copyright notice. */
-	public static final String copyrightNotice = "(C) Copyright IBM Corp. 2007, 2014";
+    /** Copyright notice. */
+    public static final String copyrightNotice = "(C) Copyright IBM Corp. 2007, 2014";
 
-	/*
-	 * Class fields
-	 */
+    /*
+     * Class fields
+     */
 
-	/** The topic (target) of the message */
-	public Object topic = null;
+    /** The topic (target) of the message */
+    public Object topic = null;
 
-	/** The message body */
-	public byte[] data = null;
+    /** The message body */
+    public byte[] data = null;
 
-	/*
-	 * Class methods
-	 */
+    /*
+     * Class methods
+     */
 
-	/**
-	 * Assigns the values of the specified message to this message.
-	 * 
-	 * @param source
-	 *            the source message.
-	 */
-	public void set(Message source) {
+    /**
+     * Assigns the values of the specified message to this message.
+     *
+     * @param source
+     *            the source message.
+     */
+    public void set(Message source) {
 
-		this.topic = source.topic;
-		this.data = source.data;
+        this.topic = source.topic;
+        this.data = source.data;
 
-	}
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
 
-		byte[] nonNullData = (data != null) ? data : new byte[0];
-		String toString = "topic [" + topic + "] data [" + new String(nonNullData) + "]";
-		return toString;
+        byte[] nonNullData = (data != null) ? data : new byte[0];
+        String toString = "[" + topic + "][" + new String(nonNullData) + "]";
+        return toString;
 
-	}
+    }
 }
