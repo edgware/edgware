d69bc82d299028bf1ea5a9ebd88d6bc804101c16
gibsoncr <gibsoncr@uk.ibm.com>
2016-04-01 11:40:30 +0100

    New interface, preparing for introduction of new wire protocol for Edgware messages.

---
+++ fabric.lib/src/fabric/bus/messages/IWireXML.java
@@ -0,0 +1,22 @@
+/*
+ * (C) Copyright IBM Corp. 2009, 2014
+ * 
+ * LICENSE: Eclipse Public License v1.0
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package fabric.bus.messages;
+
+/**
+ * Interface defining constants for the Fabric XML wire protocol.
+ */
+public interface IWireXML {
+
+	/** Copyright notice. */
+	public static final String copyrightNotice = "(C) Copyright IBM Corp. 2009, 2014";
+
+	/*
+	 * Class constants
+	 */
+
+}
