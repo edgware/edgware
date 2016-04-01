92713f2cee8db91129b8ca90e8482a2e0657b155
gibsoncr <gibsoncr@uk.ibm.com>
2016-04-01 12:39:46 +0100

    1. Update header comments. 2. Improvements to logging messages.

--- fabric.services.proxypublisher/src/fabric/services/proxypublisher/ProxyPublisherService.java
+++ fabric.services.proxypublisher/src/fabric/services/proxypublisher/ProxyPublisherService.java
@@ -1,8 +1,6 @@
 /*
- * Licensed Materials - Property of IBM
- *  
  * (C) Copyright IBM Corp. 2012
- * 
+ *
  * LICENSE: Eclipse Public License v1.0
  * http://www.eclipse.org/legal/epl-v10.html
  */
@@ -30,95 +28,95 @@ import fabric.core.io.OutputTopic;
  */
 public class ProxyPublisherService extends BusService {
 
-	/** Copyright notice. */
-	public static final String copyrightNotice = "(C) Copyright IBM Corp. 2012";
+    /** Copyright notice. */
+    public static final String copyrightNotice = "(C) Copyright IBM Corp. 2012";
 
-	/*
-	 * Class static fields
-	 */
+    /*
+     * Class static fields
+     */
 
-	/*
-	 * Class fields
-	 */
+    /*
+     * Class fields
+     */
 
-	/** A local copy of the interface to Fabric management functions. */
-	private IBusServices busServices = null;
+    /** A local copy of the interface to Fabric management functions. */
+    private IBusServices busServices = null;
 
-	/*
-	 * Class methods
-	 */
+    /*
+     * Class methods
+     */
 
-	/**
-	 * Constructs a new instance.
-	 */
-	public ProxyPublisherService() {
+    /**
+     * Constructs a new instance.
+     */
+    public ProxyPublisherService() {
 
-		super();
+        super();
 
-	}
+    }
 
-	/**
-	 * @see fabric.bus.services.impl.BusService#initService(fabric.bus.plugins.IPluginConfig)
-	 */
-	@Override
-	public void initService(IPluginConfig config) {
+    /**
+     * @see fabric.bus.services.impl.BusService#initService(fabric.bus.plugins.IPluginConfig)
+     */
+    @Override
+    public void initService(IPluginConfig config) {
 
-		super.initService(config);
+        super.initService(config);
 
-		/* Make a local copy of the accessor for Fabric management services */
-		busServices = ((IBusServiceConfig) config).getFabricServices();
+        /* Make a local copy of the accessor for Fabric management services */
+        busServices = ((IBusServiceConfig) config).getFabricServices();
 
-	}
+    }
 
-	/**
-	 * @see fabric.bus.services.IService#handleServiceMessage(fabric.bus.messages.IServiceMessage,INotificationMessage,
-	 *      IClientNotificationMessage[])
-	 */
-	@Override
-	public IServiceMessage handleServiceMessage(IServiceMessage serviceMessage, INotificationMessage responseMessage,
-			IClientNotificationMessage[] clientResponses) throws Exception {
+    /**
+     * @see fabric.bus.services.IService#handleServiceMessage(fabric.bus.messages.IServiceMessage,INotificationMessage,
+     *      IClientNotificationMessage[])
+     */
+    @Override
+    public IServiceMessage handleServiceMessage(IServiceMessage serviceMessage, INotificationMessage responseMessage,
+            IClientNotificationMessage[] clientResponses) throws Exception {
 
-		/* Get the payload to publish */
-		IMessagePayload payloadToPublish = serviceMessage.getPayload();
+        /* Get the payload to publish */
+        IMessagePayload payloadToPublish = serviceMessage.getPayload();
 
-		/* Build the feed message */
-		IFeedMessage feedMessage = busServices.busIO().wrapRawMessage(new byte[1], false);
+        /* Build the feed message */
+        IFeedMessage feedMessage = busServices.busIO().wrapRawMessage(new byte[1], false);
 
-		/* Add the payload */
-		feedMessage.setPayload((IMessagePayload) payloadToPublish.replicate());
+        /* Add the payload */
+        feedMessage.setPayload((IMessagePayload) payloadToPublish.replicate());
 
-		/* Add the correlation ID (if any) */
-		String correlationID = serviceMessage.getCorrelationID();
-		if (correlationID != null) {
-			feedMessage.setCorrelationID(correlationID);
-		}
+        /* Add the correlation ID (if any) */
+        String correlationID = serviceMessage.getCorrelationID();
+        if (correlationID != null) {
+            feedMessage.setCorrelationID(correlationID);
+        }
 
-		/* Add the properties */
-		MessageProperties properties = (MessageProperties) serviceMessage.getProperties().replicate();
-		feedMessage.setProperties(properties);
+        /* Add the properties */
+        MessageProperties properties = (MessageProperties) serviceMessage.getProperties().replicate();
+        feedMessage.setProperties(properties);
 
-		/* Get the bus channel and base topic (if any) */
-		SharedChannel busChannel = busServices.ioChannels().receiveBusChannel;
-		InputTopic busBaseTopic = busServices.ioChannels().receiveBus;
+        /* Get the bus channel and base topic (if any) */
+        SharedChannel busChannel = busServices.ioChannels().receiveBusChannel;
+        InputTopic busBaseTopic = busServices.ioChannels().receiveBus;
 
-		/* Build the bus topic to which the payload is to be published */
-		OutputTopic busTopic = new OutputTopic(busBaseTopic.name() + '/'
-				+ serviceMessage.getProperty(IServiceMessage.PROPERTY_DELIVER_TO_FEED));
+        /* Build the bus topic to which the payload is to be published */
+        OutputTopic busTopic = new OutputTopic(busBaseTopic.name() + '/'
+                + serviceMessage.getProperty(IServiceMessage.PROPERTY_DELIVER_TO_FEED));
 
-		/* Publish the message */
-		logger.log(Level.FINEST, "Publishing message to bus topic \"{0}\":\n{1}", new Object[] {busTopic,
-				feedMessage.toString()});
-		busChannel.write(feedMessage.toWireBytes(), busTopic);
+        /* Publish the message */
+        logger.log(Level.FINEST, "Publishing message to bus topic \"{0}\":\n{1}", new Object[] {busTopic,
+                feedMessage.toString()});
+        busChannel.write(feedMessage.toWireBytes(), busTopic);
 
-		return serviceMessage;
+        return serviceMessage;
 
-	}
+    }
 
-	/**
-	 * @see fabric.bus.services.IPersistentService#stopService()
-	 */
-	public void stopService() {
+    /**
+     * @see fabric.bus.services.IPersistentService#stopService()
+     */
+    public void stopService() {
 
-		logger.log(Level.FINE, "Service stopped: {0}", getClass().getName());
-	}
+        logger.log(Level.FINE, "Service [{0}] stopped", getClass().getName());
+    }
 }
