cff49ee8644a45a98d1db843e566e679c87b8dff
gibsoncr <gibsoncr@uk.ibm.com>
2016-04-14 12:11:40 +0100

    Minor formatting changes for clarity.

--- fabric.admin/src/fabric/tools/json/JsonCLIResponseListener.java
+++ fabric.admin/src/fabric/tools/json/JsonCLIResponseListener.java
@@ -1,28 +1,28 @@
 package fabric.tools.json;
 
 public class JsonCLIResponseListener extends Thread {
-	
-	JsonClient jsonClient = null;
-	long timeout = 100000; 
-	
-	public JsonCLIResponseListener(JsonClient jsonClient) {
-		this.jsonClient = jsonClient;
-	}
-	
-	@Override
-	public synchronized void run() {
-		while (true)
-		{
-			try {
-				String response = jsonClient.getResponse(timeout);
-				if (response != null ) {
-					System.out.println("RESPONSE : " + response);
-				}
-			} catch (InterruptedException e) {
-				
-			//Expected just loop again
-			}
-		}
-	}
+
+    JsonClient jsonClient = null;
+    long timeout = 100000;
+
+    public JsonCLIResponseListener(JsonClient jsonClient) {
+        this.jsonClient = jsonClient;
+        setName("JSON-CLI-Response-Listener");
+    }
+
+    @Override
+    public synchronized void run() {
+        while (true) {
+            try {
+                String response = jsonClient.getResponse(timeout);
+                if (response != null) {
+                    System.out.println("RESPONSE : " + response);
+                }
+            } catch (InterruptedException e) {
+
+                // Expected just loop again
+            }
+        }
+    }
 
 }
